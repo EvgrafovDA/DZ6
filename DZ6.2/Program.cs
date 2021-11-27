0043f114726fa1f801462b892c8a0d3e3037d1dd
Дмитрий Евграфов <EvgrafovDA@yandex.ru>
2021-11-27 18:21:54 +0100

    Добавьте файлы проекта.

---
+++ DZ6.2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DZ6._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = Console.ReadLine();
+            str = str.Replace(" ", string.Empty);
+            string reverseStr = ""; 
+            for (int i = str.Length - 1; i >= 0; i--)
+            {                
+                reverseStr += str[i];                
+            }
+            if (str == reverseStr)
+                Console.WriteLine("Введенная строка является палиндромом.");
+            else
+                Console.WriteLine("Введенная строка не является палиндромом.");
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ DZ6/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DZ6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите строку: ");
+            string input = Console.ReadLine();
+            string[] str = input.Split(new Char[] { ' ', ',', '.', ':', '!', '?', ';' }, StringSplitOptions.RemoveEmptyEntries);
+            int maxlen = 0, index = 0, count = 0;
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (str[i].Length > maxlen)
+                {
+                    maxlen = str[i].Length;
+                    index = i;
+                }
+
+
+            }
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (str[i] == str[index]) count++;
+            }
+
+            Console.Write("Самое длинное слово: {0}; Встретилось раз:{1} ", str[index], count);
+
+            Console.ReadLine();
+        }
+    }
+}
